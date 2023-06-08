6b0234c02ec48f83917172d8f26b2474922dae4e
Maxim <maxim.skrypnyk@unibw.de>
2023-06-08 20:58:17 +0200

    rebuild initialisation pv_power

--- modules/bezug_sungrow/main.sh
+++ modules/bezug_sungrow/main.sh
@@ -10,12 +10,8 @@ else
 	MYLOGFILE="$RAMDISKDIR/evu.log"
 fi
 
-if [[ "$pvwattmodul" == "wr_sungrow" ]]; then
-	echo "value read at pv modul" >/dev/null
-else
-	bash "$OPENWBBASEDIR/packages/legacy_run.sh" "modules.devices.sungrow.device" "counter" "$pv1_ipa" "$pv1_ida" "$sungrowsr" >>"$MYLOGFILE" 2>&1
-	ret=$?
-fi
+bash "$OPENWBBASEDIR/packages/legacy_run.sh" "modules.devices.sungrow.device" "counter" "$pv1_ipa" "$pv1_ida" "$sungrowsr" >>"$MYLOGFILE" 2>&1
+ret=$?
 
 openwbDebugLog $DMOD 2 "EVU RET: $ret"
 
